aed22395dd579897ffc5821d27a6461efa61f5a4
Horfi <grudka@interia.pl>
2022-04-14 00:58:49 +0200

    up

--- First game/Assets/Scripts/main_block.cs
+++ First game/Assets/Scripts/main_block.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mainblock : MonoBehaviour
+public class main_block : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
