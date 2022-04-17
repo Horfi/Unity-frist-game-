0692cdbf33ddd72c895bdcc7da7cff3899a0c8a5
Horfi <grudka@interia.pl>
2022-04-17 23:33:52 +0200

    +

---
+++ First game/Assets/Scripts/GameOverScrean.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScrean : MonoBehaviour
+{
+    public Text pointsText;
+    public void Setup(int score)
+    {
+        if (1 > 0)
+        {
+            gameObject.SetActive(true);
+        }
+        
+    }
+}
--- First game/Assets/Scripts/start_force_ball.cs
+++ First game/Assets/Scripts/start_force_ball.cs
@@ -9,16 +9,54 @@ public class start_force_ball : MonoBehaviour
     {
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(Vector3.down*15, ForceMode.Impulse);
-        rigidbody.AddForce(Vector3.right, ForceMode.Impulse);
+        rigidbody.AddForce(Vector3.right, ForceMode.Impulse); 
     }
 
     // Update is called once per frame
     void Update()
     {
     }
-    private void OnCollisionEnter(Collision collision)
+    int golden = 0;
+    int ruby = 0;
+    public int number_of_blocks = 21;
+    public static void F_golden(int golden)
+    {
+
+    }
+    public void OnCollisionEnter(Collision collision)
     {
         GetComponent<Rigidbody>().AddForce(GetComponent<Rigidbody>().velocity/110, ForceMode.Impulse);
+        if (collision.collider.tag == "golden")
+        {
+            if (collision.contactCount == 1)
+            {
+                golden++;
+                if (golden == 3)
+                {
+                    Destroy(collision.gameObject);
+                    number_of_blocks--;
+                }
+            }
+        }
+        if (collision.collider.tag == "ruby")
+        {
+            if (collision.contactCount == 1)
+            {
+                ruby++;
+                if (ruby == 5)
+                {
+                    Destroy(collision.gameObject);
+                    number_of_blocks--;
+                }
+            }
+        }
+        if (collision.collider.tag == "block")
+        {
+            Destroy(collision.gameObject);
+            number_of_blocks--;
+        }
     }
+
+    
 
 }
